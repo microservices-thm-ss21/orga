aaecbe99c3549a34a2c4ae00d88b0a6a07816cd9
Dominik Kröll <dominik.kroell@mni.thm.de>
2021-04-22 10:01:57 +0200

    devenv

--- pull-script.sh
+++ pull-script.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
-path=$(dirname "$(readlink -f "$0")")
+maindir=${pwd}
 # equals ./orga
-cd $path
+
 git pull
 
-cd ./../issue-service
-git pull
- 
-cd ./../project-service
-git pull
- 
-cd ./../template-service
+cd ../issue-service
 git pull
 
-cd ./../user-service
+cd ../project-service
+git pull
+
+cd ../template-service
+git pull
+
+cd ../user-service
 git pull
